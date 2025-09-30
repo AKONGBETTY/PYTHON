72dce2bc8d39fdcb2a8b77daa47f8dcae0ec6e52
Betty <akongbetty2@gmail.com>
2025-09-30 12:07:50 +0100

    reverse

---
+++ reverse.py
@@ -0,0 +1,11 @@
+
+
+sentence = input("Enter a group of words: ")
+words = sentence.split()
+reversed_sentence = ' '.join(reversed(words))
+print("Reversed:", reversed_sentence)
+
+
+word = input("Enter a word: ")
+reversed_word = word[::-1]
+print("Reversed word:", reversed_word)
